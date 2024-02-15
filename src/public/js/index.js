8c4049fab4f1b1953b0981a876599418139a0edb
CRKJ <0soundlive@gmail.com>
2024-02-15 12:28:59 -0500

    ++ renta

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,6 +1,6 @@
 console.log("%cNodeJS 2023", "font-size:2rem; color:lime;");
 
-const version = 2023
+const version = 2023;
 
 const header = document.getElementById("header_container");
 const footer = document.getElementById("footer_container");
@@ -26,13 +26,13 @@ header.innerHTML = `
 </nav>`;
 
 footer.innerHTML = `
-  <p class="text1">App para registrar las ventas de <strong>Pinkylin Moda Intima</strong></p>
-  <p class="text2">Creada por <strong>@CRKJ</strong> para Pinkylin Moda Intima 😎👍 ${version}</p>
-  <p class="tech_text">
-    <img src="/img/mysql.svg" />
-    <img src="/img/html5.svg" />
-    <img src="/img/css3.svg" />
-    <img src="/img/javascript.svg" />
-    <img src="/img/nodejs.svg" />
-  </p>
+<p class="tech_text">
+  <img src="/img/mysql.svg" />
+  <img src="/img/html5.svg" />
+  <img src="/img/css3.svg" />
+  <img src="/img/javascript.svg" />
+  <img src="/img/nodejs.svg" />
+</p>
+<p class="text1">App para registrar las ventas de <strong>Pinkylin Moda Intima</strong></p>
+<p class="text2">Creada por <strong>@CRKJ</strong> para Pinkylin Moda Intima 😎👍 ${version}</p>
 `;
